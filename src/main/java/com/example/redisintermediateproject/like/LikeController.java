c77c82720d60eef90d833e05f6811679c82ba3a3
KimJayhyun <dnlwjem000@gmail.com>
2026-04-26 03:20:37 +0900

    실습 2

--- src/main/java/com/example/redisintermediateproject/like/LikeController.java
+++ src/main/java/com/example/redisintermediateproject/like/LikeController.java
@@ -1,10 +1,11 @@
 package com.example.redisintermediateproject.like;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,9 +15,12 @@ public class LikeController {
     private final LikeService likeService;
 
     @PostMapping()
-    public void likePost(
-        @RequestBody LikePostRequestDto likePostRequestDto
-    ) {
+    public void likePost(@RequestBody LikePostRequestDto likePostRequestDto) {
         likeService.likePost(likePostRequestDto);
+    }
+
+    @PostMapping("/redis")
+    public void likePostWithRedis(@RequestBody LikePostRequestDto likePostRequestDto) {
+        likeService.likePostWithRedis(likePostRequestDto);
     }
 }
