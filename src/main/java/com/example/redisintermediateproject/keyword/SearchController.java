7fac0772ef0ba6147fdb093da0da42bd1e6564d7
KimJayhyun <dnlwjem000@gmail.com>
2026-04-26 11:27:47 +0900

    Section 4 - Sorted Set

--- src/main/java/com/example/redisintermediateproject/keyword/SearchController.java
+++ src/main/java/com/example/redisintermediateproject/keyword/SearchController.java
@@ -1,12 +1,13 @@
 package com.example.redisintermediateproject.keyword;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +24,10 @@ public class SearchController {
     @GetMapping("/top10")
     public List<String> getTop10Keywords() {
         return searchService.getTop10Keywords();
+    }
+
+    @GetMapping("/top10/redis")
+    public List<String> getTop10KeywordsWithRedis() {
+        return searchService.getTop10KeywordsWithRedis();
     }
 }
