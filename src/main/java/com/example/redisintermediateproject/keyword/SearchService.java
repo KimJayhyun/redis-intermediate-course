0db46e455361df524945ea6428739df7178de599
KimJayhyun <79262624+KimJayhyun@users.noreply.github.com>
2026-04-26 11:28:15 +0900

    Section 4 - Sorted Set (#3)

--- src/main/java/com/example/redisintermediateproject/keyword/SearchService.java
+++ src/main/java/com/example/redisintermediateproject/keyword/SearchService.java
@@ -1,21 +1,24 @@
 package com.example.redisintermediateproject.keyword;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class SearchService {
 
     private final SearchRepository searchRepository;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Transactional
     public void search(String keyword) {
-        SearchKeyword searchKeyword = searchRepository.findByKeyword(keyword)
-                .orElse(new SearchKeyword(keyword));
+        SearchKeyword searchKeyword =
+                searchRepository.findByKeyword(keyword).orElse(new SearchKeyword(keyword));
         searchKeyword.increaseCount();
         searchRepository.save(searchKeyword);
     }
@@ -23,7 +26,18 @@ public class SearchService {
     @Transactional(readOnly = true)
     public List<String> getTop10Keywords() {
         return searchRepository.findTop10ByOrderByCountDesc().stream()
-                .map(SearchKeyword::getKeyword)
+                .map(SearchKeyword::getKeyword).toList();
+    }
+
+    public void seatchWithRedis(String keyword) {
+        // ZINCRBY [search_keyword_ranking] 1 [keyword]
+        redisTemplate.opsForZSet().incrementScore("search_keyword_ranking", keyword, 1.0);
+    }
+
+    public List<String> getTop10KeywordsWithRedis() {
+        // ZREVRANGE [search_keyword_ranking] 0 9
+        // ZRANGE [search_keyword_ranking] 0 9 REV
+        return redisTemplate.opsForZSet().reverseRange("search_keyword_ranking", 0, 9).stream()
                 .toList();
     }
 }
