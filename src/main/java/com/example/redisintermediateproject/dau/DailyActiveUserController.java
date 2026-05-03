a99161e16ee4316478d6f9984a8c269eec8a232b
KimJayhyun <79262624+KimJayhyun@users.noreply.github.com>
2026-05-03 10:42:33 +0900

    Section 7 - Set & Bitmap으로 DAU 카운팅 (#6)
    * Daily Activate User - Set: O(1)
    
    * Daily Activate User - Bitmap: O(N)

--- src/main/java/com/example/redisintermediateproject/dau/DailyActiveUserController.java
+++ src/main/java/com/example/redisintermediateproject/dau/DailyActiveUserController.java
@@ -1,7 +1,7 @@
 package com.example.redisintermediateproject.dau;
 
 import java.time.LocalDate;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/dau")
@@ -19,19 +21,25 @@ public class DailyActiveUserController {
 
   // 활동 유저 기록 API
   @PostMapping("/record")
-  public void recordActiveUser(
-      @RequestBody RecordActiveUserRequestDto recordActiveUserRequestDto
-  ) {
+  public void recordActiveUser(@RequestBody RecordActiveUserRequestDto recordActiveUserRequestDto) {
     dailyActiveUserService.recordActiveUser(recordActiveUserRequestDto.getUserId());
   }
 
   // DAU 조회 API
   @GetMapping("/count")
-  public long getDau(
-      @RequestParam
-      @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-      LocalDate date
-  ) {
+  public long getDau(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
     return dailyActiveUserService.getDau(date);
+  }
+
+  @PostMapping("/record/redis")
+  public void recordActiveUserWithRedis(
+      @RequestBody RecordActiveUserRequestDto recordActiveUserRequestDto) {
+    dailyActiveUserService.recordActiveUserWithRedis(recordActiveUserRequestDto.getUserId());
+  }
+
+  @GetMapping("/count/redis")
+  public long getDauWithRedis(
+      @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+    return dailyActiveUserService.getDauWithRedis(date);
   }
 }
--- src/main/java/com/example/redisintermediateproject/dau/DailyActiveUserService.java
+++ src/main/java/com/example/redisintermediateproject/dau/DailyActiveUserService.java
@@ -1,15 +1,20 @@
 package com.example.redisintermediateproject.dau;
 
 import java.time.LocalDate;
-import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class DailyActiveUserService {
 
   private final DailyActiveUserRepository dailyActiveUserRepository;
+  private final RedisTemplate<String, String> redisTemplate;
 
   @Transactional
   public void recordActiveUser(Long userId) {
@@ -25,5 +30,25 @@ public class DailyActiveUserService {
   public long getDau(LocalDate date) {
     // 특정 날짜의 DAU를 SQL문의 COUNT를 활용해 계산
     return dailyActiveUserRepository.countByActiveDate(date);
+  }
+
+  public void recordActiveUserWithRedis(Long userId) {
+    LocalDate today = LocalDate.now();
+    String key = "dau:" + today.toString(); // dau:2024-06-01
+
+    // SETBIT [key] [offset] [value]
+    redisTemplate.opsForValue().setBit(key, userId, true);
+
+    // DAU 데이터는 하루 동안만 유효하므로, 키의 유효 기간을 1일로 설정
+    redisTemplate.expire(key, 1, java.util.concurrent.TimeUnit.DAYS);
+  }
+
+  public long getDauWithRedis(LocalDate date) {
+    String key = "dau:" + date.toString();
+
+    // Redis에 저장된 비트맵을 활용하여 DAU를 계산
+    // Redis 명령어의 'BITCOUNT [key]'와 동일하다.
+    return redisTemplate
+        .execute((RedisCallback<Long>) (connection) -> connection.bitCount(key.getBytes()));
   }
 }
